9b7b3171d84293e4afedde9eb144e415c4350794
Aravinth Veeramuthu <aravinthvvs1@gmail.com>
2022-12-20 16:57:04 +0530

    modified BS for ascending and descending order of List

--- GoLang/DataStructure/BinarySearch.go
+++ GoLang/DataStructure/BinarySearch.go
@@ -3,23 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(BinarySearch([]int{1,2,3,4,5,6,7},1))
+	fmt.Println(BinarySearch([]int{7, 6, 5, 4, 3, 2, 1}, 4))
+	fmt.Println(BinarySearch([]int{1, 2, 3, 4, 5, 6, 7}, -3))
 }
 
 func BinarySearch(list []int, target int) bool {
+	isAssending := true
+	if list[0] > list[len(list)-1] {
+		isAssending = false
+	}
 
 	low := 0
-	high := len(list)-1
+	high := len(list) - 1
 
-	for low <= high{
+	for low <= high {
 
 		mid := low + (high-low)/2
-		if list[mid] == target{
+		if list[mid] == target {
 			return true
-		}else if list[mid] < target{
-			low = mid +1
-		}else{
-			high = mid -1
+		}
+
+		if isAssending {
+			if list[mid] < target {
+				low = mid + 1
+			} else {
+				high = mid - 1
+			}
+		} else {
+			if list[mid] > target {
+				low = mid + 1
+			} else {
+				high = mid - 1
+			}
 		}
 	}
 	return false
